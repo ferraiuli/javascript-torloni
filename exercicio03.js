c59582751af26ac0584d403555ee05a1d167b4c9
GUSTAVO FERRAIULI <54701867845@imaccommand025.local>
2025-11-19 14:49:36 -0300

    impar-par

---
+++ exercicio03.js
@@ -0,0 +1,31 @@
+let numero = parseInt(prompt("digite o numero e veja se ele é par ou impar:"));
+
+if (numero % 2 == 0){
+    alert("O número " + numero + " é par.");
+} else {
+    alert("O número " + numero + " é impar.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
