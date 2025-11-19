d3bbcf16a90dbbded569636b14cb4b10462f8eb6
GUSTAVO FERRAIULI <54701867845@imaccommand025.local>
2025-11-19 13:39:34 -0300

    senha e usuario

--- exercicio01.js
+++ exercicio01.js
@@ -1,25 +1,36 @@
+// let usuario = prompt("digite o seu nome de usuario: ")
+
+// switch (usuario) {
+//     case "admin":
+//         console.log("usuario correto")
+//         break;
+
+//     default:
+//         console.log("usuario incorreto")
+//         break;
+// }
+
+// let senha = prompt(" digite a senha: ")
+
+// switch (senha) {
+//     case "senha123":
+//         console.log("senha correta, bem vindo admin!")
+//         break;
+
+// default:
+//         console.log("senha incorreta")
+//         break;
+
+// }
+// window.alert("login bem-sucedido" )
+
 let usuario = prompt("digite o seu nome de usuario: ")
+let senha = prompt("digite sua senha")
 
-switch (usuario) {
-    case "admin":
-        console.log("usuario correto")
-        break;
-
-    default:
-        console.log("usuario incorreto")
-        break;
+if (usuario == "admin" && senha == "senha123" ) {
+    console.log("login bem-sucedido")
+    
 }
-
-let senha = prompt(" digite a senha: ")
-
-switch (senha) {
-    case "senha123":
-        console.log("senha correta, bem vindo admin!")
-        break;
-
-default:
-        console.log("senha incorreta")
-        break;
-
+else{
+    console.log("login incorreto! ;-;")
 }
-window.alert("login bem-sucedido" )
