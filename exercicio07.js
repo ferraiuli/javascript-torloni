d837e0c150d849a36def8dc77712c6cf07c73363
GUSTAVO FERRAIULI <54701867845@imaccommand025.local>
2025-11-26 08:43:20 -0300

    aplicando desconto

---
+++ exercicio07.js
@@ -0,0 +1,18 @@
+
+let valorproduto = Number(prompt("Digite o valor total da compra: "));
+
+let desconto = 0;
+
+if (valorproduto <= 100) {
+    desconto = 0;
+} else if (valorproduto <= 200) {
+    desconto = valorproduto * 0.10;
+} else {
+    desconto = valorproduto * 0.20;
+}
+
+let Total = valorproduto - desconto;
+
+alert("Valor da compra: R$ " + valorproduto.toFixed(2) +
+      "\nDesconto: R$ " + desconto.toFixed(2) +
+      "\nValor Total: R$ " + Total.toFixed(2));
